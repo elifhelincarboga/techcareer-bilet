068df80602abe94a320a6b578d824f445e5620b3
ELIF HELIN CARBOGA <elifhelincarboga@gmail.com>
2023-11-11 14:35:38 +0300

    feature: Backend body-parser added to project

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 8080
+
+//Middlewares
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!!')
