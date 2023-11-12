119ab2376bb2298db8e61a3d2a2a27ca48f98788
ELIF HELIN CARBOGA <elifhelincarboga@gmail.com>
2023-11-12 16:46:34 +0300

    Bugfix: Added cors library for fix the cors errors

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const routes = require('./routes')
 const db = require('./db')
+const cors = require('cors')
 require('dotenv/config')
 const app = express()
 
@@ -10,6 +11,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 //Middlewares
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
