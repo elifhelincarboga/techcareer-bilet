520249c2a31620db4ea0b2323443690ef7593718
ELIF HELIN CARBOGA <elifhelincarboga@gmail.com>
2023-12-10 16:33:52 +0300

    Feature: Add admin page, Set private routes

--- frontend/src/router/index.jsx
+++ frontend/src/router/index.jsx
@@ -1,4 +1,5 @@
-import { createBrowserRouter } from "react-router-dom"
+import { createBrowserRouter, Navigate, Route, Routes } from "react-router-dom"
+import { useSelector } from "react-redux"
 
 import App from "../App"
 import Home from "../pages/Home"
@@ -7,6 +8,22 @@ import EventDetail from "../pages/EventDetail"
 import PastEvents from "../pages/PastEvents"
 import Login from "../pages/Login"
 import SignUp from "../pages/Signup"
+import Admin from "../pages/Admin"
+
+// eslint-disable-next-line react/prop-types
+const PrivateRoute = ({ component: Component }) => {
+  const token = useSelector((state) => state.auth.accessToken)
+
+  if (!token) {
+    return <Navigate to="/" />
+  }
+
+  return (
+    <Routes>
+      <Route path="/" element={<Component />} />
+    </Routes>
+  )
+}
 
 const router = createBrowserRouter([
   {
@@ -28,6 +45,10 @@ const router = createBrowserRouter([
       {
         path: "/event/:eventId",
         element: <EventDetail />
+      },
+      {
+        path: "/admin",
+        element: <PrivateRoute component={Admin} />
       }
     ]
   },
